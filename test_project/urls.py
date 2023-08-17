47481d51a3b4e5f311570db91fe49cd52bd35d64
Musa Adamu Wakili <musawakiliml@hotmail.com>
2023-08-17 15:23:51 +0100

    feat: Added login feature

--- test_project/urls.py
+++ test_project/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('', include('pages.urls')),
     path('', include('posts.urls')),
     path('', include('blog.urls')),
